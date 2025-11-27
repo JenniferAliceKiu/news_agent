7476d9d13fa74be5d43868929352dcc50262b975
Jennifer Kiunke <jennifer.alice.kiunke@gmail.com>
2025-11-27 11:12:48 +0100

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,56 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+require 'faker'
+
+puts "Cleaning database..."
+#Chat.destroy_all
+Message.destroy_all
+Chat.destroy_all
+Daily.destroy_all
+
+User.destroy_all
+
+
+puts "Creating Instances..."
+puts 'creating users'
+1.times do
+  users = User.new(
+    email: Faker::Internet.email, #(find or create by)
+    password: 'password123'
+  )
+  users.save!
+  end
+puts 'Users done'
+
+puts 'creating dailies'
+10.times do
+  dailies = Daily.new(
+    title: Faker::Lorem.sentence(word_count: 5),
+    summary: Faker::Lorem.paragraph(sentence_count: 3)
+  )
+  dailies.save!
+  end
+puts 'Dailies done'
+
+###
+
+puts 'creating chats'
+10.times do
+  chats = Chat.new(
+    name: Faker::Lorem.words(number: 3).join(' '),
+    user: User.last,
+    daily: Daily.last
+  )
+  chats.save!
+  end
+puts 'Chats done'
+
+
+puts 'creating messages'
+25.times do
+  messages = Message.new(
+    content: Faker::Lorem.sentence(word_count: 10),
+    direction: ['incoming', 'outgoing'].sample,
+    chat: Chat.last
+  )
+  messages.save!
+  end
+puts 'Messages done'
